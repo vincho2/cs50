c9c2d7d954a5cf47e544317e52080a4f5c68c983
vincho2 <vinkent@gmail.com>
2025-09-15 13:34:00 +0000

    Mon, Sep 15, 2025, 3:34 PM +02:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -199,6 +199,7 @@ void add_pairs(void)
     // Update pair count when loop is over
     pair_count = k + 1;
 
+    printf("Unsorted array populated:\n");
     for (int i = 0; i < pair_count; i++)
     {
         printf("Winner: %s vs Loser: %s, Votes: %i\n",
