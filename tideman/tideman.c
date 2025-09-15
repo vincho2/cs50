30e1ede09091b210ea6b61f67896f94b0daf6131
vincho2 <vinkent@gmail.com>
2025-09-15 13:23:56 +0000

    Mon, Sep 15, 2025, 3:23 PM +02:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -319,7 +319,7 @@ void sort_array(pair pairs_array[], int array_size)
             }
             else
             {
-                for (int i = 0; i < array_size)
+                for (int i = 0; i < array_size; i++)
                 {
                     printf("Winner: %s vs Loser: %s, Votes: %i\n",
                         candidates[pairs[i].winner], candidates[pairs[i].loser], preferences[pairs[i].winner][pairs[i].loser]);
