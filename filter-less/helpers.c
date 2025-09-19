9287dc2fea48e1d3de554fd62dcd612f33974e37
vincho2 <vinkent@gmail.com>
2025-09-19 16:07:28 +0000

    Fri, Sep 19, 2025, 6:07 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -3,6 +3,14 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            if ()
+        }
+    }
+
     return;
 }
 
