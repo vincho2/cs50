190559a4f988c180b69fd3af40b6b13ebb306b5a
vincho2 <vinkent@gmail.com>
2025-09-19 17:08:18 +0000

    Fri, Sep 19, 2025, 7:08 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -55,7 +55,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             gptr = &image[i][j].rgbtGreen;
             rptr = &image[i][j].rgbtRed;
 
-            sepiaBlue = set_sepia_color(, &image[i][j])
+            sepiaBlue = set_sepia_color(BLUE, &image[i][j])
 
             sepiaBlue = (BYTE) fmax(.272 * (*rptr) + .534 * (*gptr) + .131 * (*bptr), 255);
             sepiaGreen = (BYTE) fmax(.349 * (*rptr) + .686 * (*gptr) + .168 * (*bptr), 255);
