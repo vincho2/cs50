c99226b4c8656f4210c8519265a67b6fe2247735
vincho2 <vinkent@gmail.com>
2025-09-19 18:17:09 +0000

    Fri, Sep 19, 2025, 8:17 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -126,7 +126,7 @@ BYTE set_sepia_color(Color c, BYTE b_input, BYTE g_input, BYTE r_input)
 
     // Compute resulting sepia color
     int int_result =
-        (int) fmax(MAX_BYTE, b_factor * b_input + g_factor * g_input + r_factor * r_input);
+        (int) fmin(MAX_BYTE, b_factor * b_input + g_factor * g_input + r_factor * r_input);
 
     // return result as a byte type
     return (BYTE) int_result;
