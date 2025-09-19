c422f58f8fbdba974ed15b42c082ac3b49dd96ae
vincho2 <vinkent@gmail.com>
2025-09-19 16:26:09 +0000

    Fri, Sep 19, 2025, 6:26 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -7,39 +7,36 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     const BYTE BLACK = 0x00;
     const BYTE WHITE = 0xff;
 
-    int pixel_brightness;
+    BYTE pixel_brightness;
 
 
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < height; j++)
         {
-            // Compute pixel brightness
-            pixel_brightness = image.rgbtBlue
-            if ()
-            {
-                image.rgbtBlue =
-            }
+            // Compute pixel brightness and assign the resulted value to each colour of the pixel
+            pixel_brightness = (image.rgbtBlue + image.rgbtGreen + image.rgbtRed) / 3;
+            image.rgbtBlue = pixel_brightness;
+            image.rgbtGreen = pixel_brightness;
+            image.rgbtRed = pixel_brightness;
         }
     }
-
-    return;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+
 }
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+
 }
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    
 }
