b8732ca70baeca39e449be19051517891fa113e7
vincho2 <vinkent@gmail.com>
2025-09-19 16:47:43 +0000

    Fri, Sep 19, 2025, 6:47 PM +02:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -11,20 +11,27 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     // Declare brightness variable
     BYTE pixel_brightness;
 
+    BYTE *blueptr;
+    BYTE *greenptr;
+    BYTE *redptr;
+
     // Loop on each row
     for (int i = 0; i < height; i++)
     {
         // Loop on each column
         for (int j = 0; j < width; j++)
         {
-            // Initialize pixel variable
-            pixel = &image[i][j];
+            // Initialize colors pointers
+            blueptr = &image[i][j].rgbtBlue;
+            greenptr = &image[i][j].rgbtGreen;
+            redptr = &image[i][j].rgbtRed;
+
             // Compute pixel brightness
-            pixel_brightness = (*pixel.rgbtBlue + *pixel.rgbtGreen + *pixel.rgbtRed) / 3;
+            pixel_brightness = (*blueptr + *greenptr + *redptr) / 3;
             // Assign the resulted brightness value to each color of the pixel
-            *pixel.rgbtBlue = pixel_brightness;
-            *pixel.rgbtGreen = pixel_brightness;
-            *pixel.rgbtRed = pixel_brightness;
+            *blueptr = pixel_brightness;
+            *greenptr = pixel_brightness;
+            *redptr = pixel_brightness;
         }
     }
 }
