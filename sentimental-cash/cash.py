bd0c8f932c9016b3183e65155a5376d51bb4969c
vincho2 <vinkent@gmail.com>
2025-10-04 06:46:55 +0000

    Sat, Oct 4, 2025, 8:46 AM +02:00

---
+++ sentimental-cash/cash.py
@@ -0,0 +1,27 @@
+from cs50 import get_float
+
+# Initialize coin list in cents and change amount
+coins = [25, 10, 5, 1]
+change = 0
+
+# Ask the user how much change is owed until he/she provides a positive float
+while True:
+    change = get_float("Change: ") * 100
+    if change > 0:
+        break
+
+# Initialise coin count
+coin_count = 0
+
+# Compute the number of coins to be given back
+for coin in coins:
+    # Stop looping when there is no change left
+    if change == 0:
+        break
+    # Count the number of coins for a given coin value
+    elif change >= coin:
+        coin_count += change // coin
+        change = change % coin
+
+# Diplay result
+print(int(coin_count))
