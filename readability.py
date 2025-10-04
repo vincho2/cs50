1511fc09ef82d875c31bc3a1faad57a3bd1bf754
vincho2 <vinkent@gmail.com>
2025-10-04 07:22:19 +0000

    Sat, Oct 4, 2025, 9:22 AM +02:00

--- readability.py
+++ readability.py
@@ -12,19 +12,37 @@ counters = {
     C_SENTENCES: 0
 }
 
+
+
 # --------------------------------------------------------------------------------------------------
 # Define main function to print the grade of an input text
 # --------------------------------------------------------------------------------------------------
-def main()
+def main():
 
+    # Ask the user for a text
+    text = input("Text: ")
 
+    # Compute the grade of the input text
+    grade = compute_grade(text)
 
-# -------------------------------------------------------------------------------------------------
+    # Display result
+    if grade < 1:
+        print('Before Grade 1')
+    elif grade > 16:
+        print('Grade 16+')
+    else:
+        print(f'Grade {grade})
+
+# --------------------------------------------------------------------------------------------------
 # Function to compute the grade
-# -------------------------------------------------------------------------------------------------
-def compute_grade(text)
+# --------------------------------------------------------------------------------------------------
+def compute_grade(text):
 
 
+# --------------------------------------------------------------------------------------------------
+# Function to update the array containing the different character types in a provided text
+# --------------------------------------------------------------------------------------------------
+def count_text_elements(text):
 
 
 # Call main
