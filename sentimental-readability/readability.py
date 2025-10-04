dcb547c7fbfc74c26f1b11074b64b4ee87091d0d
vincho2 <vinkent@gmail.com>
2025-10-04 08:01:40 +0000

    Sat, Oct 4, 2025, 10:01 AM +02:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,68 +1,37 @@
+# Define sentences separators
+separators = ['.', '?', '!']
+
+# Ask the user for a text
+text = input("Text: ")
+
 # Initialize counters as global variables
 words = 0
 letters = 0
 sentences = 0
 
-# Define sentences separators
-separators = ['.', '?', '!']
+# Loop on each text caracter and update counters
+for char in text:
 
+    # Count letters
+    if char.isalnum():
+        letters += 1
+    elif char.isspace():
+        words += 1
+    elif char in separators:
+        sentences += 1
 
-# --------------------------------------------------------------------------------------------------
-# Define main function to print the grade of an input text
-# --------------------------------------------------------------------------------------------------
-def main():
+# L = average number of letters per 100 words in the text
+L = letters * 100 / words
+# S = average number of sentences per 100 words in the text
+S = sentences * 100 / words
 
-    # Ask the user for a text
-    text = input("Text: ")
+# Return Coleman-Liau index
+grade = 0.0588 * L - 0.296 * S - 15.8
 
-    # Count text elements and store them in the dedicated global variables
-    count_text_elements(text)
-
-    # Compute the grade of the input text
-    grade = compute_grade(text)
-
-    # Display result
-    if grade < 1:
-        print('Before Grade 1')
-    elif grade > 16:
-        print('Grade 16+')
-    else:
-        print(f'Grade {grade}')
-
-
-# --------------------------------------------------------------------------------------------------
-# Function to compute the grade
-# --------------------------------------------------------------------------------------------------
-def compute_grade(text):
-
-    # L = average number of letters per 100 words in the text
-    L = letters * 100 / words
-    # S = average number of sentences per 100 words in the text
-    S = sentences * 100 / words
-
-    # Return Coleman-Liau index
-    return 0.0588 * L - 0.296 * S - 15.8
-
-
-# --------------------------------------------------------------------------------------------------
-# Function to update the array containing the different character types in a provided text
-# --------------------------------------------------------------------------------------------------
-def count_text_elements(text):
-
-    # Loop on each text caracter and update counters
-    for char in text:
-
-        # Count letters
-        if char.isalnum():
-            letters += 1
-        elif char.isspace():
-            words += 1
-        elif char in separators:
-            sentences += 1
-
-
-# --------------------------------------------------------------------------------------------------
-# Call main
-# --------------------------------------------------------------------------------------------------
-if __name__ == "__main__":
-    main()
+# Display result
+if grade < 1:
+    print('Before Grade 1')
+elif grade > 16:
+    print('Grade 16+')
+else:
+    print(f'Grade {grade}')
