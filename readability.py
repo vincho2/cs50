25c0626cb7699d4982893e1f9b9d3a172f6a9578
vincho2 <vinkent@gmail.com>
2025-10-04 07:48:55 +0000

    Sat, Oct 4, 2025, 9:48 AM +02:00

--- readability.py
+++ readability.py
@@ -1,18 +1,3 @@
-
-
-# Define counters
-C_WORDS = 'word count'
-C_LETTERS = 'letter count'
-C_SENTENCES = 'sentence count'
-
-# Define counter dictionary
-counters = {
-    C_WORDS: 0,
-    C_LETTERS: 0,
-    C_SENTENCES: 0
-}
-
-
 # Initialize counters as global variables
 words = 0
 letters = 0
