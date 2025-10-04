f4ebf33d7cf56e9a66b51cbe9c9b3ab9b02613da
vincho2 <vinkent@gmail.com>
2025-10-04 15:15:04 +0000

    Sat, Oct 4, 2025, 5:15 PM +02:00

--- dna/dna.py
+++ dna/dna.py
@@ -7,7 +7,7 @@ def main():
     # TODO: Check for command-line usage
 
     # TODO: Read database file into a variable
-    
+
     # TODO: Read DNA sequence file into a variable
 
     # TODO: Find longest match of each STR in DNA sequence
@@ -43,16 +43,16 @@ def longest_match(sequence, subsequence):
             # If there is a match in the substring
             if sequence[start:end] == subsequence:
                 count += 1
-            
+
             # If there is no match in the substring
             else:
                 break
-        
+
         # Update most consecutive matches found
         longest_run = max(longest_run, count)
 
     # After checking for runs at each character in seqeuence, return longest run found
     return longest_run
 
-
+if __name__
 main()
