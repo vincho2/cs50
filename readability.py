096e6357558ed2784a5e71f7e3b4409389b2d3f2
vincho2 <vinkent@gmail.com>
2025-10-04 07:06:03 +0000

    Sat, Oct 4, 2025, 9:06 AM +02:00

--- readability.py
+++ readability.py
@@ -1,9 +1,24 @@
 
 
-C_WORDS = 
+# Define counters
+C_WORDS = 'word count'
+C_LETTERS = 'letter count'
+C_SENTENCES = 'sentence count'
 
-
+# Define counter dictionary
 counters = {
-
-
+    C_WORDS: 0,
+    C_LETTERS: 0,
+    C_SENTENCES: 0
 }
+
+def main()
+
+
+
+
+
+
+# Call main
+if __name__ == "__main__":
+    main()
