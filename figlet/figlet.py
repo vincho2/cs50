2aaf5fe402e43079db8f228cee60b830f5277feb
vincho2 <vinkent@gmail.com>
2025-10-05 19:44:44 +0000

    Sun, Oct 5, 2025, 9:44 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,32 +1,49 @@
 from pyfiglet import Figlet
+import sys
 import argparse
 from random import choice
 
-# Initialise pyfiglet
+# --------------------------------------------------------------------------------------------------
+# Generic invalid usage function
+# --------------------------------------------------------------------------------------------------
+def return_exit():
+    print('Invalid usage')
+    sys.exit(1)
+
+# --------------------------------------------------------------------------------------------------
+# Main code
+# --------------------------------------------------------------------------------------------------
+
+# Get fonts into a list
 figlet = Figlet()
 font_list = figlet.getFonts()
 
-# Création du parser d'arguments
-parser = argparse.ArgumentParser(description="Render text in a random or specified font")
-parser.add_argument(
-    "-f", "--font",
-    type=str,
-    help="Font to use (must be one of the available fonts)"
-)
+# Customize argument parser to display a generic message when exiting on error
+class MyArgumentParser(argparse.ArgumentParser):
+    def error(self, message):
+        return_exit()
 
-# Analyse des arguments
+# Define the parser and font argument
+parser = MyArgumentParser(description='Display text in a random or specified font')
+parser.add_argument('-f', '--font', help = 'Font to be used (must be in the available fonts)')
+
+# Create arguments namespace
 args = parser.parse_args()
 
-# Choix de la font
+# Set the font if argument is present
 if args.font:
+    # Make sure the font exists
     if args.font in font_list:
         figlet.setFont(font=args.font)
+    # Otherwise exit the program
     else:
-        print(f"Error: '{args.font}' is not a valid font.")
-        exit(1)
+        return_exit()
+# Set the font to a random one if not specified
 else:
-    figlet.setFont(font=choice(font_list))
+    figlet.setFont(font = choice(font_list))
 
-# Saisie du texte et affichage
-text = input("Input: ")
+# Get input text and print it in the font
+text = input('Input: ')
 print(figlet.renderText(text))
+
+
