c61fc74d3f2a7509949f15b75b851c58b3595969
vincho2 <vinkent@gmail.com>
2025-10-05 15:52:10 +0000

    Sun, Oct 5, 2025, 5:52 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,8 +1,8 @@
 import sys
 
 while True:
-    if len(sys.argv) in [1, 3]
+    if len(sys.argv) == 1 or (len(sys.argv) == 3 and argv]
        break
-    
+    print('Usage: python figlet.py with no argument or figlet.py -f)
 
     input ('Input: ')
