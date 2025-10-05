5ec8dce1a2dec509eefc1c3902212f1ac81f3958
vincho2 <vinkent@gmail.com>
2025-10-05 16:30:00 +0000

    Sun, Oct 5, 2025, 6:30 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,12 +1,31 @@
 from pyfiglet import Figlet
 import sys
 
+def main():
+
+    if len(sys.argv) == 1:
+        text = get_input()
+
+
+
+
+
 figlet = Figlet()
 font_list = figlet.getFonts()
+bool random_font = len(sys.argv) == 1
+bool selected_font = (len(sys.argv) == 3 and sys.argv[2] in ('-f', '--font') and sys.argv[3] in font_list)
 
-if len(sys.argv) != 1 and (len(sys.argv) != 3 or sys.argv[2] not in ('-f', '--font') or sys.argv[3] not in font_list):
+if  and :
 
     print('Invalid usage')
     sys.exit()
 
-input('Input: ')
+
+
+
+def get_input():
+    return input('Input: ')
+
+
+def 
+
