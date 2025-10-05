d7820ada0c78161364b8eaebeac909587f27a606
vincho2 <vinkent@gmail.com>
2025-10-05 16:22:27 +0000

    Sun, Oct 5, 2025, 6:22 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -4,10 +4,9 @@ import sys
 figlet = Figlet()
 font_list = figlet.getFonts()
 
-if len(sys.argv) == 1 or (len(sys.argv) == 3 and argv[]
-       break
+if len(sys.argv) != 1 and (len(sys.argv) != 3 or sys.argv[2] not in ('-f', '--font') or sys.argv[3] not in font_list):
 
-    print('Usage: python figlet.py with no argument or figlet.py -f')
+    print('Invalid usage')
     sys.exit()
 
-    input ('Input: ')
+input('Input: ')
