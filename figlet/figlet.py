15e9725546080e74132ae19d92a08dd704024cde
vincho2 <vinkent@gmail.com>
2025-10-05 19:29:36 +0000

    Sun, Oct 5, 2025, 9:29 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -3,10 +3,18 @@ import sys
 import argparse
 from random import choice
 
+def return_exit():
+    print('Invalid usage')
+    sys.exit(1)
+
 figlet = Figlet()
 font_list = figlet.getFonts()
 
-parser = argparse.ArgumentParser(description='Display text in a random or specified font')
+class MyArgumentParser(argparse.ArgumentParser):
+    def error(self, message):
+        return_exit()
+
+parser = MyArgumentParser(description='Display text in a random or specified font')
 parser.add_argument('-f', '--font', help = 'Font to be used (must be in the available fonts)')
 
 args = parser.parse_args
@@ -17,24 +25,9 @@ if args.font:
     else:
         return_exit()
 else:
-
-
-
-
-if len(sys.argv) == 1:
     figlet.setFont(font = choice(font_list))
-
-elif len(sys.argv) == 3 and sys.argv[1] in ('-f', '--font') and sys.argv[2] in font_list:
-    figlet.setFont(font=sys.argv[2])
-
-else:
-    print('Invalid usage')
-    sys.exit(1)
 
 text = input('Input: ')
 print(figlet.renderText(text))
 
 
-def return_exit():
-    print('Invalid usage')
-    sys.exit(1)
