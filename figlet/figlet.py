f0b07345a7ceea21571fe52199255bccb60f069d
vincho2 <vinkent@gmail.com>
2025-10-05 17:02:35 +0000

    Sun, Oct 5, 2025, 7:02 PM +02:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -8,8 +8,8 @@ font_list = figlet.getFonts()
 if len(sys.argv) == 1:
     figlet.setFont(font = choice(font_list))
 
-elif len(sys.argv) == 3 and sys.argv[2] in ('-f', '--font') and sys.argv[3] in font_list:
-    figlet.setFont(font=sys.argv[3])
+elif len(sys.argv) == 3 and sys.argv[1] in ('-f', '--font') and sys.argv[2] in font_list:
+    figlet.setFont(font=sys.argv[2])
 
 else:
     print('Invalid usage')
