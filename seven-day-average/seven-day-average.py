44313717a6ac18357b936e5c31c26378ee23fbc6
vincho2 <vinkent@gmail.com>
2025-10-06 16:25:51 +0000

    Mon, Oct 6, 2025, 6:25 PM +02:00

--- seven-day-average/seven-day-average.py
+++ seven-day-average/seven-day-average.py
@@ -34,6 +34,7 @@ def main():
 
 # TODO: Create a dictionary to store 14 most recent days of new cases by state
 def calculate(reader):
+    today = max(reader.)
 
     result = {
         'Washington': 23,
